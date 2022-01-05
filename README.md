7d3cf048c40cdbbe72ed2fa86f855704b39c822a
jentrialgo <jentrialgo@gmail.com>
2022-01-05 18:19:34 +0100

    Add support for Wordpress

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
 # Blog book
 
-Blog book is a python scripts that converts blogspot blogs into PDF files.
+Blog book is a python scripts that converts Blogspot or Wordpress blogs into PDF
+files to be printed like a book. It uses their RSS feed.
 
 # Usage
 
 ```bash
-python blogbook.py --base-url=https://example.blogspot.com
+python blogbook.py --base-url https://example.blogspot.com --platform Blogspot
+```
+
+See all options with:
+
+```bash
+python blogbook.py --help
+
+Usage: blogbook.py [OPTIONS]
+
+Options:
+  --base-url TEXT                 Base URL of the blog. For instance:
+                                  http://example.blogspot.com  [required]
+  --output TEXT                   Output file name  [default: output.pdf]
+  --platform [Blogspot|Wordpress]
+                                  Platform used to host the blog  [required]
+  --help                          Show this message and exit.
 ```
--- blogbook.py
+++ blogbook.py
@@ -3,31 +3,42 @@ import feedparser
 import weasyprint
 import click
 from rich.progress import Progress
+from rich.console import Console
 
 import locale
 locale.setlocale(locale.LC_ALL, '')
 
-def get_blogspot_entries(base_url):
-
-    feed_url = base_url + "/feeds/posts/default/"
-
+def get_entries(base_url, platform):
+    if platform.lower() == "blogspot":
+        feed_url = base_url + "/feeds/posts/default/"
+    else:
+        feed_url = base_url + "/feed/atom/"
 
     index = 1
+    page = 1
     all_entries = []
     with Progress(transient=True) as progress:
         while True:
-            url = f"{feed_url}?start-index={index}"
+            if platform.lower() == "blogspot":
+                url = f"{feed_url}?start-index={index}"
+            else:
+                url = f"{feed_url}?paged={page}"
+
             feed = feedparser.parse(url)
             
-            total_results = int(feed.feed.opensearch_totalresults)
-            if index == 1:
-                task = progress.add_task("Downloading feed", total=total_results)
+            if platform.lower() == "blogspot":
+                total_results = int(feed.feed.opensearch_totalresults)
+                if index == 1:
+                    task = progress.add_task("Downloading feed", total=total_results)
+
+                progress.advance(task, len(feed.entries))
+            else:  # For wordpress
+                print(f"Downloading page {page}")
 
             all_entries.extend(feed.entries)
 
             index += len(feed.entries)
-
-            progress.advance(task, len(feed.entries))
+            page += 1
 
             if len(feed.entries) == 0:
                 break
@@ -47,7 +58,7 @@ def print_entry_summary(entries):
         entry_index += 1
 
 
-def convert_to_pdf(entries, output_filename):
+def convert_to_pdf(entries, platform, output_filename,):
     entry_index = 1
     all_text = ""
     for entry in entries:
@@ -57,24 +68,32 @@ def convert_to_pdf(entries, output_filename):
         print(f"[{entry_index}] ({entry.published[:10]}) {entry.title} {tags}")
         entry_index += 1
 
-        date = datetime.fromisoformat(entry.published)
-        date_str = date.strftime("%c").capitalize()
+        if platform.lower() == 'blogspot':
+            date = datetime.fromisoformat(entry.published)
+            date_str = date.strftime("%c").capitalize()
+        else:  # Wordpress
+            date_str = entry.published
         headers = f"<h1>{entry.title}</h1>\n\n<h2>{date_str}</h2>"
         all_text += headers  + entry.content[0].value + "\n\n"
 
     if not output_filename.endswith(".pdf"):
         output_filename += ".pdf"
 
-    weasyprint.HTML(string=all_text).write_pdf(output_filename)
+    console = Console()
+    with console.status("Generating PDF...") as status:
+        weasyprint.HTML(string=all_text).write_pdf(output_filename)
 
 @click.command()
 @click.option('--base-url', type=str, required=True,
     help='Base URL of the blog. For instance: http://example.blogspot.com')
 @click.option('--output', type=str, default="output.pdf", show_default=True,
     help='Output file name')
-def main(base_url, output):
-    entries = get_blogspot_entries(base_url)
-    convert_to_pdf(entries, output)
+@click.option('--platform', type=click.Choice(['Blogspot', "Wordpress"],
+    case_sensitive=False), required=True,
+    help='Platform used to host the blog')
+def main(base_url, output, platform):
+    entries = get_entries(base_url, platform)
+    convert_to_pdf(entries, platform, output)
 
 
 if __name__ == "__main__":
